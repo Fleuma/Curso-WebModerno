1c95b3256585564048930d15ceb69f08f9df2860
Fleuma <91841910+Fleuma@users.noreply.github.com>
2022-02-04 22:13:12 -0300

    param em function

---
+++ exercicios-js/funcoes/paramvariaveis.js
@@ -0,0 +1,13 @@
+function soma() {
+    let soma = 0
+    for (i in arguments) { // Arguments é necessário quando não se tem certeza da quantidade de valores que o usuário usará na function.
+        soma+= arguments[i] // Soma os valores até que não haja mais
+    }
+    return soma
+}
+
+console.log(soma())
+console.log(soma(1))
+console.log(soma(1.1, 2.2, 3.3))
+console.log(soma(1.1, 2.2, 'Teste'))
+console.log(soma('a', 'b', 'c'))
